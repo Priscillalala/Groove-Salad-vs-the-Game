6c1f157b0e3c76f942910226d5b3784c1ab80b42
Groove Salad <95263024+Priscillalala@users.noreply.github.com>
2025-01-19 22:13:55 -0800

    finish balloons

--- GSvs/Assets/GSvs/RoR2/Items/SaleStarChestAttachment.cs
+++ GSvs/Assets/GSvs/RoR2/Items/SaleStarChestAttachment.cs
@@ -32,6 +32,26 @@ namespace GSvs.RoR2.Items
             }
         }
 
+        public void OnChestOpenedServer()
+        {
+            Vector3 origin = transform.position;
+            if (chestServer && chestServer.dropTransform)
+            {
+                origin = chestServer.dropTransform.position;
+            }
+            else if (rouletteChestServer && rouletteChestServer.ejectionTransform)
+            {
+                origin = rouletteChestServer.ejectionTransform.position;
+            }
+            EffectManager.SpawnEffect(openingEfectPrefab, new EffectData
+            {
+                origin = origin,
+                rotation = transform.rotation,
+            }, true);
+
+            Destroy(gameObject);
+        }
+
         public void Start()
         {
             if (balloons)
@@ -61,39 +81,48 @@ namespace GSvs.RoR2.Items
             if (balloons)
             {
                 balloons.transform.SetParent(null);
-                var force = balloons.forceOverLifetime;
+
+                /*var force = balloons.forceOverLifetime;
                 force.enabled = true;
-                ParticleSystem.Particle[] balloonParticles = new ParticleSystem.Particle[balloons.particleCount];
+                var limitVelocityOverLifeTime = balloons.limitVelocityOverLifetime;
+                limitVelocityOverLifeTime.enabled = true;*/
+                var collision = balloons.collision;
+                collision.enabled = true;
+
+                /*ParticleSystem.Particle[] balloonParticles = new ParticleSystem.Particle[balloons.particleCount];
                 balloons.GetParticles(balloonParticles);
                 for (int i = 0; i < balloons.particleCount; i++)
                 {
                     ref ParticleSystem.Particle particle = ref balloonParticles[i];
-                    particle.remainingLifetime = 10f;
-                    //particle.position = particle.position;
-                    //particle.velocity = particle.velocity;
+                    particle.remainingLifetime = 30f;
+                    Vector3 velocity = (particle.position - transform.position).normalized * 5f;
+                    velocity.y *= 0.5f;
+                    particle.velocity = velocity;
                 }
-                balloons.SetParticles(balloonParticles);
-            }
-        }
+                balloons.SetParticles(balloonParticles);*/
 
-        public void OnChestOpenedServer()
-        {
-            Vector3 origin = transform.position;
-            if (chestServer && chestServer.dropTransform)
-            {
-                origin = chestServer.dropTransform.position;
-            }
-            else if (rouletteChestServer && rouletteChestServer.ejectionTransform)
-            {
-                origin = rouletteChestServer.ejectionTransform.position;
-            }
-            EffectManager.SpawnEffect(openingEfectPrefab, new EffectData
-            {
-                origin = origin,
-                rotation = transform.rotation,
-            }, true);
+                if (balloons.TryGetComponent(out Rigidbody rigidbody))
+                {
+                    rigidbody.AddForce(Vector3.up, ForceMode.Impulse);
+                }
 
-            Destroy(gameObject);
+                if (balloons.TryGetComponent(out ConstantForce constantForce))
+                {
+                    constantForce.enabled = true;
+                }
+                /*if (balloons.TryGetComponent(out DestroyOnParticleEnd destroyOnParticleEnd))
+                {
+                    destroyOnParticleEnd.enabled = true;
+                }*/
+                if (balloons.TryGetComponent(out DestroyOnTimer destroyOnTimer))
+                {
+                    destroyOnTimer.enabled = true;
+                }
+                if (balloons.TryGetComponent(out Collider collider))
+                {
+                    collider.enabled = true;
+                }
+            }
         }
     }
 }
